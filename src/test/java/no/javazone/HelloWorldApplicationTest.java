7225a25d30662a617327503e38a5cb5bb72b68b1
Henrik Nordvik <henrikno@gmail.com>
2015-05-05 23:21:26 +0200

    Legger til en systemtest.

---
+++ src/test/java/no/javazone/HelloWorldApplicationTest.java
@@ -0,0 +1,31 @@
+package no.javazone;
+
+import io.dropwizard.testing.ConfigOverride;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloWorldApplicationTest {
+
+    @ClassRule
+    public static final DropwizardAppRule<HelloWorldConfiguration> RULE =
+            new DropwizardAppRule<>(HelloWorldApplication.class,
+                    ResourceHelpers.resourceFilePath("test-configuration.yaml"),
+                    ConfigOverride.config("server.applicationConnectors[0].port", "9090"));
+
+    @Test
+    public void server_svarer_200_ok() {
+        WebTarget resource = ClientBuilder.newClient().target("http://localhost:" + RULE.getLocalPort() + "/hello-world");
+
+        final Response response = resource.request().get();
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
+}
