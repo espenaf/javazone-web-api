2c32962220c0ed4172a17b48748b55092faa43d8
Henrik Nordvik <henrikno@gmail.com>
2015-05-05 20:04:57 +0200

    Add hello world web server.

---
+++ src/main/java/no/javazone/HelloWorldApplication.java
@@ -0,0 +1,35 @@
+package no.javazone;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            args = new String[]{"server", "configuration.yaml"};
+        }
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(
+            HelloWorldConfiguration configuration,
+            Environment environment
+    ) {
+        final HelloWorldResource resource = new HelloWorldResource();
+        environment.jersey().register(resource);
+    }
+
+}
